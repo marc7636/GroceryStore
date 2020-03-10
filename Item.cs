7f836b599577ce44c08547a04c95a002ec245050
Daniel R <daniel.reffeldt@outlook.com>
2020-03-10 14:48:14 +0100

    Made item.cs

---
+++ Item.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore
+{
+    class Item {
+
+        string name;
+
+        public DateTime ExpirationDate {
+            get {
+
+            }
+            set {
+
+            }
+        }
+
+        float price;
+    }
+}
